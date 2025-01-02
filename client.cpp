1cd890d7932229fea982885cde33a7da221e4cb6
Josiahgodfrey <rusanyujosiah7@gmail.com>
2025-01-02 05:57:02 -0800

    advancing commit

--- client.cpp
+++ client.cpp
@@ -20,5 +20,14 @@ int main() {
     struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
 
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        cerr << "Socket creation error: " << strerror(errno) << endl;
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
     return 0;
 }
